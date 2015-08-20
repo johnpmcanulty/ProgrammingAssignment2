1bcb435481165f35ba3fe5f8a3344d747b138581
John McAnulty <johnpmcanulty@gmail.com>
2015-08-19 22:26:25 -0400

    Initial change to test commit;

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following R functions create a cached matrix for efficiency
+## in processing data by using environment variables manipulated
+## using lexical scoping.
 
-## Write a short comment describing this function
+## The makeCacheMatrix R function creates a matrix object
+## in the environment for efficient processing via other functions, 
+## such as the cacheSolve R function below.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve R function returns a matrix that is the inverse of the one
+## set as an environment variable using lexical scoping by the makeCacheMatrix
+# R function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
